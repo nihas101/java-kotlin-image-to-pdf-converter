b6499a894cb59253471504ca7134e7e4e80616c5
Nikita Hasert <nikitahasert@gmail.com>
2018-02-02 21:32:04 +0100

    Add constants

---
+++ src/main/java/de/nihas101/imagesToPdfConverter/Constants.java
@@ -0,0 +1,8 @@
+package de.nihas101.imagesToPdfConverter;
+
+public class Constants{
+    public final static Double CELL_SIZE = 100.0;
+    public final static int LIST_CELL_MAX_STRING_LENGTH = 27;
+    public final static int NOTIFICATION_MAX_STRING_LENGTH = 75;
+    public final static String DIRECTORY_IMAGE_PATH = "src/main/resources/if_2-Folder_2672696.png";
+}
