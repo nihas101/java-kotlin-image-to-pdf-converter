4976aff90b1f2876ef1fc7a64cf3e32d65e93ec9
Nikita Hasert <nikitahasert@gmail.com>
2018-02-02 22:32:49 +0100

    Add iterator for image files

---
+++ src/main/kotlin/de/nihas101/imagesToPdfConverter/fileReader/ImageFilesIterator.kt
@@ -0,0 +1,30 @@
+package de.nihas101.imagesToPdfConverter.fileReader
+
+import java.io.File
+import javax.imageio.ImageIO
+
+class ImageFilesIterator private constructor(private val directory: File): DirectoryIterator {
+    private var files: List<File> =
+                        if(directory.isDirectory) directory.listFiles().filter { file -> isImage(file) }
+                        else List(1, { _ -> directory }).filter { file -> isImage(file) }
+    private var currentIndex = 0
+
+    companion object ImageFilesIteratorFactory {
+         fun createImagesDirLoader(directory: File): ImageFilesIterator = ImageFilesIterator(directory)
+    }
+
+    override fun getFile(index: Int): File = files[index]
+
+    override fun getFiles(): List<File> = files
+
+    override fun nextFile(): File{
+        if(currentIndex < files.size) return files[currentIndex++]
+        throw NoMoreImagesException(directory)
+    }
+
+    override fun nrOfFiles(): Int = files.size
+
+    override fun getParentDirectory(): File = directory
+
+    private fun isImage(file: File): Boolean = file.isFile && (ImageIO.read(file) != null)
+}
