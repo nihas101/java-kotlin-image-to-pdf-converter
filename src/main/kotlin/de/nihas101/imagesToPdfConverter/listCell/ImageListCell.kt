73628a30bb1a3c298e76a79b580ab328a458b8da
Nikita Hasert <nikitahasert@gmail.com>
2018-02-02 22:34:47 +0100

    Add image list cell class For displaying images and their file names in a listview

---
+++ src/main/kotlin/de/nihas101/imagesToPdfConverter/listCell/ImageListCell.kt
@@ -0,0 +1,56 @@
+package de.nihas101.imagesToPdfConverter.listCell
+
+import de.nihas101.imagesToPdfConverter.Constants.*
+import javafx.geometry.Pos
+import javafx.scene.control.ListCell
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+import java.io.File
+
+class ImageListCell(private val imageMap: Map<String, Image>) : ListCell<File>(){
+    private val imageView: ImageView = ImageView()
+    private val directoryImageString = File(DIRECTORY_IMAGE_PATH).toURI().toURL().toString()
+
+    override fun updateItem(file: File?, empty: Boolean) {
+        super.updateItem(file, empty)
+        if (empty || file == null) {
+            text = null
+            graphic = null
+        } else {
+            loadImage(file)
+        }
+    }
+
+    private fun loadImage(file: File?) {
+        if(file!!.isDirectory)
+            imageView.image = imageMap[directoryImageString]
+        else
+            imageView.image = imageMap[file.toURI().toURL().toString()]
+
+        if(imageView.image != null) scaleImageView(imageView)
+
+        graphic = createVBox(imageView, cropText(file.name))
+    }
+
+    private fun scaleImageView(imageView: ImageView){
+        val scale = (CELL_SIZE /imageView.image.height)
+        imageView.fitHeight = imageView.image.height * scale
+        imageView.fitWidth  = imageView.image.width * scale
+    }
+
+    private fun createVBox(imageView: ImageView, text: Text): VBox{
+        val vBox = VBox()
+        vBox.children.addAll(imageView, text)
+        vBox.alignment = Pos.CENTER
+        return vBox
+    }
+
+    private fun cropText(text: String): Text{
+        return if(text.length > LIST_CELL_MAX_STRING_LENGTH)
+            Text(text.substring(0,LIST_CELL_MAX_STRING_LENGTH) + "...")
+        else
+            Text(text)
+    }
+}
