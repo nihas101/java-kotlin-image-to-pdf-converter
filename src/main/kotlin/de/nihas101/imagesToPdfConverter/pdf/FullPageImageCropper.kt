159cc407c16a482b1891072bf0c810e54826a0c2
Nikita Hasert <nikitahasert@gmail.com>
2018-02-02 21:39:38 +0100

    Add class for cropping pages to fit images

---
+++ src/main/kotlin/de/nihas101/imagesToPdfConverter/pdf/FullPageImageCropper.kt
@@ -0,0 +1,35 @@
+package de.nihas101.imagesToPdfConverter.pdf
+
+import com.itextpdf.kernel.pdf.PdfArray
+import com.itextpdf.kernel.pdf.PdfName
+import com.itextpdf.kernel.pdf.PdfPage
+import com.itextpdf.layout.element.Image
+
+class FullPageImageCropper : ImagePdfPageFormatter(){
+    private val lowerLeftX: Float = 0F
+
+    companion object FullPageCropperFactory{
+        fun createFullPageCropper(): FullPageImageCropper = FullPageImageCropper()
+    }
+
+    override fun format(pdfPage: PdfPage, image: Image){
+        val pdfRatio = (pdfPage.mediaBox.width / pdfPage.mediaBox.height)
+        val imageRatio = (image.imageWidth / image.imageHeight)
+        val scaleBetweenPdfAndImage = pdfRatio / imageRatio
+        val cropBoxArray = createCropBoxArray(
+                lowerLeftX,
+                calculateBottomCrop(pdfPage.mediaBox.height, scaleBetweenPdfAndImage),
+                pdfPage.mediaBox.width,
+                pdfPage.mediaBox.height
+        )
+
+        pdfPage.pdfObject.put(PdfName.CropBox, cropBoxArray)
+    }
+
+    private fun createCropBoxArray(lowerLeftX: Float, lowerLeftY: Float, upperRightX: Float, upperRightY: Float): PdfArray {
+        return PdfArray(floatArrayOf(lowerLeftX,lowerLeftY,upperRightX,upperRightY))
+    }
+
+    private fun calculateBottomCrop(height: Float, scale: Float): Float = Math.abs(height - (height * scale))
+
+}
