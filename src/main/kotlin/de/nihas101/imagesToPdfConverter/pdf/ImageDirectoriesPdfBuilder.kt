06ef824808cef2a647193c21ff96b801af56de71
Nikita Hasert <nikitahasert@gmail.com>
2018-02-03 10:53:06 +0100

    Fix wrong filenames when building multiple PDFs

--- src/main/kotlin/de/nihas101/imagesToPdfConverter/pdf/ImageDirectoriesPdfBuilder.kt
+++ src/main/kotlin/de/nihas101/imagesToPdfConverter/pdf/ImageDirectoriesPdfBuilder.kt
@@ -4,6 +4,8 @@ import de.nihas101.imagesToPdfConverter.ProgressUpdater
 import de.nihas101.imagesToPdfConverter.fileReader.DirectoryIterator
 import de.nihas101.imagesToPdfConverter.fileReader.ImageDirectoriesIterator
 import de.nihas101.imagesToPdfConverter.pdf.ImagePdfBuilder.PdfBuilderFactory.createPdfImageBuilder
+import java.io.File
+import java.nio.file.Paths
 
 class ImageDirectoriesPdfBuilder {
     companion object PdfBuilderFactory {
@@ -18,7 +20,10 @@ class ImageDirectoriesPdfBuilder {
     }
 
     fun build(imageDirectoriesIterator: ImageDirectoriesIterator, progressUpdater: ProgressUpdater){
-        val nrOfFiles = imageDirectoriesIterator.nrOfFiles()
+        val nrOfFiles = imageDirectoriesIterator.getFiles().size
+        if(nrOfFiles == 0) return
+
+        /* TODO: Maybe build them in multiple Threads? */
 
         for (i in 0 until nrOfFiles){
             buildNextPDF(imageDirectoriesIterator.nextImageFilesIterator())
@@ -27,7 +32,9 @@ class ImageDirectoriesPdfBuilder {
     }
 
     private fun buildNextPDF(directoryIterator: DirectoryIterator){
-        if(directoryIterator.nrOfFiles() != 0)
-            createPdfImageBuilder().build(directoryIterator, directoryIterator.getParentDirectory())
+        if(directoryIterator.nrOfFiles() != 0) {
+            val file = Paths.get(directoryIterator.getParentDirectory().absolutePath + ".pdf").toFile()
+            createPdfImageBuilder().build(directoryIterator, file)
+        }
     }
 }
