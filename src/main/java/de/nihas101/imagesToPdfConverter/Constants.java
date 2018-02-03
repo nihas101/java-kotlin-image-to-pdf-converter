4d32e619913d997f8800aefd98832597bc3b1aeb
Nikita Hasert <nikitahasert@gmail.com>
2018-02-03 10:46:22 +0100

    Add margin float

--- src/main/java/de/nihas101/imagesToPdfConverter/Constants.java
+++ src/main/java/de/nihas101/imagesToPdfConverter/Constants.java
@@ -4,5 +4,6 @@ public class Constants{
     public final static Double CELL_SIZE = 100.0;
     public final static int LIST_CELL_MAX_STRING_LENGTH = 27;
     public final static int NOTIFICATION_MAX_STRING_LENGTH = 75;
+    public static final Float NO_MARGIN = 0F;
     public final static String DIRECTORY_IMAGE_PATH = "src/main/resources/if_2-Folder_2672696.png";
 }
