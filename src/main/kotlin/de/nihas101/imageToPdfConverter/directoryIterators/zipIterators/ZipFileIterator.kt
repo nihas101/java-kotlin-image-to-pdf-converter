9172bd1d08a5e1705e79e629acc151705d34de3c
Nikita Hasert <nikitahasert@gmail.com>
2018-02-22 18:57:57 +0100

    Implement interface methods

--- src/main/kotlin/de/nihas101/imageToPdfConverter/directoryIterators/zipIterators/ZipFileIterator.kt
+++ src/main/kotlin/de/nihas101/imageToPdfConverter/directoryIterators/zipIterators/ZipFileIterator.kt
@@ -1,58 +1,50 @@
 package de.nihas101.imageToPdfConverter.directoryIterators.zipIterators
 
 import de.nihas101.imageToPdfConverter.directoryIterators.DirectoryIterator
+import de.nihas101.imageToPdfConverter.directoryIterators.imageIterators.ImageFilesIterator
+import de.nihas101.imageToPdfConverter.directoryIterators.zipIterators.Unzipper.ZipFileIteratorFactory.createFileOutputStream
+import de.nihas101.imageToPdfConverter.directoryIterators.zipIterators.Unzipper.ZipFileIteratorFactory.createUnzipper
 import java.io.File
 
 
-class ZipFileIterator(private val unzipper: Unzipper) : DirectoryIterator {
+class ZipFileIterator(private val file: File) : DirectoryIterator {
+    private val imageFilesIterator: ImageFilesIterator
+
+    init {
+        createUnzipper(file).unzip { fileName -> createFileOutputStream("${file.parent}/$fileName") }
+        imageFilesIterator = ImageFilesIterator.createImageFilesIterator(file)
+    }
 
     /* TODO: Create ImageFilesIterator and pass the newly unzipped files to it */
     /* TODO: Make it so ImageDirectoriesIterator and ImageFilesIterator can delete a folder after creating a PDF */
     /* TODO: Then add that as an option! for the user */
 
-    override fun nextFile(): File {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun nextFile() = imageFilesIterator.nextFile()
 
-    override fun getFile(index: Int): File {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getFile(index: Int) = imageFilesIterator.getFile(index)
 
-    override fun getFiles(): MutableList<File> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getFiles() = imageFilesIterator.getFiles()
 
-    override fun remove(file: File): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun remove(file: File) = imageFilesIterator.remove(file)
 
-    override fun add(index: Int, file: File): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun add(index: Int, file: File) = imageFilesIterator.add(index, file)
 
-    override fun add(file: File): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
 
-    override fun addAll(files: List<File>): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun add(file: File) = imageFilesIterator.add(file)
 
-    override fun nrOfFiles(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun addAll(files: List<File>) = imageFilesIterator.add(file)
 
-    override fun getParentDirectory(): File {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun numberOfFiles() = imageFilesIterator.numberOfFiles()
 
-    override fun resetIndex() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun getParentDirectory() = imageFilesIterator.getParentDirectory()
+
+    override fun resetIndex(){
+        imageFilesIterator.resetIndex()
     }
 
     companion object ZipFileIteratorFactory {
         fun createZipFileIterator(file: File): ZipFileIterator {
-            return ZipFileIterator(Unzipper.createUnzipper(file))
+            return ZipFileIterator(file)
         }
     }
 }
