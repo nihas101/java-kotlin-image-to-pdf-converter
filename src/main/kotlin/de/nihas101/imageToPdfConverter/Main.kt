3111699b274173e7a7cee6aab3a3690c41761323
Nikita Hasert <nikitahasert@gmail.com>
2018-02-26 17:05:51 +0100

    Add TODO

--- src/main/kotlin/de/nihas101/imageToPdfConverter/Main.kt
+++ src/main/kotlin/de/nihas101/imageToPdfConverter/Main.kt
@@ -10,6 +10,7 @@ import java.io.InputStreamReader
 
 object Main {
     /* TODO: Add copyright notice to every file */
+    /* TODO: Move all Threads to tasks! */
     @JvmStatic
     fun main(args: Array<String>) {
         if (args.isEmpty()) MainWindow().show()
