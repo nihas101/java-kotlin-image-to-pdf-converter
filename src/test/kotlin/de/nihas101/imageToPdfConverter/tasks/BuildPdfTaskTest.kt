39b134c6b31c3658cbf38b50183e2df55e3401fd
Nikita Hasert <nikitahasert@gmail.com>
2018-02-27 13:21:57 +0100

    Revert "Attempt to fix javafx test failing"
    This reverts commit 5c35a6b1fed57e8f925651b8bc560c614b4cbfc5.

--- src/test/kotlin/de/nihas101/imageToPdfConverter/tasks/BuildPdfTaskTest.kt
+++ src/test/kotlin/de/nihas101/imageToPdfConverter/tasks/BuildPdfTaskTest.kt
@@ -1,6 +1,5 @@
 package de.nihas101.imageToPdfConverter.tasks
 
-import de.nihas101.imageToPdfConverter.ToolkitInitializer.Companion.initalizeToolkit
 import de.nihas101.imageToPdfConverter.directoryIterators.DirectoryIterator.DirectoryIteratorFactory.createDirectoryIterator
 import de.nihas101.imageToPdfConverter.pdf.builders.PdfBuilder.PdfBuilderFactory.createPdfBBuilder
 import de.nihas101.imageToPdfConverter.pdf.pdfOptions.ImageToPdfOptions.OptionsFactory.createOptions
@@ -38,36 +37,5 @@ class BuildPdfTaskTest {
         assertEquals(true, beforeExecuted)
         assertEquals(true, afterExecuted)
         assertEquals(true, targetFile.exists())
-    }
-
-    @Test
-    fun cancelTask() {
-        var beforeExecuted = false
-        var afterExecuted = true
-        val imageToPdfOptions = createOptions()
-        val sourceFile = File("src/test/resources/images")
-        val targetFile = File("src/test/resources/images/buildTaskTest.pdf")
-        targetFile.createNewFile()
-        targetFile.deleteOnExit()
-        imageToPdfOptions.setSaveLocation(targetFile)
-
-        initalizeToolkit()
-
-        val buildPdfTask = createBuildPdfTask(
-                createPdfBBuilder(imageToPdfOptions.getIteratorOptions()),
-                createDirectoryIterator(sourceFile, imageToPdfOptions.getIteratorOptions()),
-                imageToPdfOptions,
-                TrivialProgressUpdater(),
-                { beforeExecuted = true },
-                { afterExecuted = true }
-        )
-
-        buildPdfTask.cancel()
-        buildPdfTask.executeTask()
-
-        assertEquals(true, beforeExecuted)
-        assertEquals(true, afterExecuted)
-        assertEquals(true, targetFile.exists())
-
     }
 }
