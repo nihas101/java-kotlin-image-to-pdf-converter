a2f4d351d664fc89d862d4c8a032cd6213fe105e
Nikita Hasert <nikitahasert@gmail.com>
2018-02-27 13:03:26 +0100

    Remove test relying on javafx toolkit

--- src/test/kotlin/de/nihas101/imageToPdfConverter/tasks/BuildPdfTaskTest.kt
+++ src/test/kotlin/de/nihas101/imageToPdfConverter/tasks/BuildPdfTaskTest.kt
@@ -5,13 +5,9 @@ import de.nihas101.imageToPdfConverter.pdf.builders.PdfBuilder.PdfBuilderFactory
 import de.nihas101.imageToPdfConverter.pdf.pdfOptions.ImageToPdfOptions.OptionsFactory.createOptions
 import de.nihas101.imageToPdfConverter.tasks.BuildPdfTask.BuildPdfTaskFactory.createBuildPdfTask
 import de.nihas101.imageToPdfConverter.util.TrivialProgressUpdater
-import javafx.embed.swing.JFXPanel
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import java.io.File
-import java.util.concurrent.CountDownLatch
-import java.util.concurrent.TimeUnit
-import javax.swing.SwingUtilities
 
 
 class BuildPdfTaskTest {
@@ -41,52 +37,5 @@ class BuildPdfTaskTest {
         assertEquals(true, beforeExecuted)
         assertEquals(true, afterExecuted)
         assertEquals(true, targetFile.exists())
-    }
-
-    @Test
-    fun cancelBuild() {
-        var beforeExecuted = false
-        val imageToPdfOptions = createOptions()
-        val pdfBuilder = createPdfBBuilder(imageToPdfOptions.getIteratorOptions())
-        val sourceFile = File("src/test/resources/images")
-        val targetFile = File("src/test/resources/images/buildTaskTestCancelled.pdf")
-        val countDownLatch = CountDownLatch(1)
-        val taskManager = TaskManager.createTaskManager()
-        targetFile.createNewFile()
-        targetFile.deleteOnExit()
-        imageToPdfOptions.setSaveLocation(targetFile)
-
-        initToolkit()
-
-        val buildPdfTask = createBuildPdfTask(
-                pdfBuilder,
-                createDirectoryIterator(sourceFile, imageToPdfOptions.getIteratorOptions()),
-                imageToPdfOptions,
-                TrivialProgressUpdater(),
-                {
-                    Thread.sleep(1000)
-                    beforeExecuted = true
-                },
-                {
-                    assertEquals(true, beforeExecuted)
-                    countDownLatch.countDown()
-                }
-        )
-
-        taskManager.start(buildPdfTask, false)
-        buildPdfTask.cancelTask()
-
-        countDownLatch.await(2, TimeUnit.SECONDS)
-        assertEquals(true, buildPdfTask.isCancelled)
-    }
-
-    private fun initToolkit() {
-        /* Source: stackoverflow.com/questions/11273773/javafx-2-1-toolkit-not-initialized */
-        val latch = CountDownLatch(1)
-        SwingUtilities.invokeLater {
-            JFXPanel() // initializes JavaFX environment
-            latch.countDown()
-        }
-        latch.await(2, TimeUnit.SECONDS)
     }
 }
