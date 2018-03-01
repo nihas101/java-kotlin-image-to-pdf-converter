681f9ea4add2e027c07714f42f97bde3f2c0913e
Nikita Hasert <nikitahasert@gmail.com>
2018-03-01 12:26:33 +0100

    Add Copyright notice

--- src/main/kotlin/de/nihas101/imageToPdfConverter/listCell/ImageListCell.kt
+++ src/main/kotlin/de/nihas101/imageToPdfConverter/listCell/ImageListCell.kt
@@ -1,3 +1,21 @@
+/*
+ Image2PDF is a program for converting images to PDFs with the use of iText 7
+ Copyright (C) 2018  Nikita Hasert
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.nihas101.imageToPdfConverter.listCell
 
 import de.nihas101.imageToPdfConverter.util.Constants.*
